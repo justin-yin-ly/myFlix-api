00005315788e5572fb4a73387364dfb7866ebc75
jl2032 <55512607+jl2032@users.noreply.github.com>
2023-09-07 16:14:46 -0400

    Reenabled authentication on the 'get all movies' enpoint

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.get('/secreturl', (req, res) => {
 });
 
 // Get full list of movies
-app.get('/movies', async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', {session: false}), async (req, res) => {
   await Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
