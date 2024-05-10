dd795dcf7ba7a954e68be43749f5359f1d678cdb
jl2032 <55512607+jl2032@users.noreply.github.com>
2024-05-10 17:44:15 -0400

    Update index.js
    This is yet another attempt at rectifying my server and addressing the CORS issue I've been seeing. So far, these past commits haven't done much and in some cases have made the problem worse, so instead I'm going to see if simplifying the CORS implementation fixes things.

--- index.js
+++ index.js
@@ -42,20 +42,7 @@ app.options('*', cors());
 // This makes it so that only specified origins in allowedOrigins can make requests
 let allowedOrigins = ['http://localhost:8080', 'http://localhost:4200', 'http://localhost:1234', 'http://testsite.com', 'https://cinedata.netlify.app', 'https://justin-yin-ly.github.io/myFlix-angular-client', 'https://justin-yin-ly.github.io/myFlix-angular-client/welcome'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn't found in the list
-      let message = `The CORS policy for this application doesn't allow access from the origin ` + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
-
-app.use(cors({
-  origin: 'https://justin-yin-ly.github.io/myFlix-angular-client/'
-}));
+app.use(cors());
 
 app.use(myLogger);
 app.use(requestTime);
